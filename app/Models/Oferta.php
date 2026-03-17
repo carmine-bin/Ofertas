8a0bd49ab2775954f78666351deda4410b379981
Carmine <256667193+carmine-bin@users.noreply.github.com>
2026-03-16 18:52:48 -0600

    rutas resource

--- app/Models/Oferta.php
+++ app/Models/Oferta.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Oferta extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'titulo', 
+        'vigencia', 
+        'tienda', 
+        'precio_original', 
+        'precio_descuento'
+    ];
 }
